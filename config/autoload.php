37f51e8676fa2dac5eff377db3a36aaa020b5422
Philipp Kaiblinger <philipp.kaiblinger@kaipo.at>
2012-09-21 09:24:50 +0200

    include autoloader

---
+++ config/autoload.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2005-2012 Leo Feyer
+ * 
+ * @package Pk_noobSlide
+ * @link    http://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+	'ContentNoobSlideEnd'     => 'system/modules/pk_noobSlide/ContentNoobSlideEnd.php',
+	'ContentNoobSlideNews'    => 'system/modules/pk_noobSlide/ContentNoobSlideNews.php',
+	'ContentNoobSlidePreview' => 'system/modules/pk_noobSlide/ContentNoobSlidePreview.php',
+	'ContentNoobSlideSection' => 'system/modules/pk_noobSlide/ContentNoobSlideSection.php',
+	'ContentNoobSlideSetup'   => 'system/modules/pk_noobSlide/ContentNoobSlideSetup.php',
+	'ModuleNoobSlideArticle'  => 'system/modules/pk_noobSlide/ModuleNoobSlideArticle.php',
+));
+
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(array
+(
+	'ce_noobslide_end'      => 'system/modules/pk_noobSlide/templates',
+	'ce_noobslide_preview'  => 'system/modules/pk_noobSlide/templates',
+	'ce_noobslide_section'  => 'system/modules/pk_noobSlide/templates',
+	'ce_noobslide_setup'    => 'system/modules/pk_noobSlide/templates',
+	'mod_noobslide_article' => 'system/modules/pk_noobSlide/templates',
+));
